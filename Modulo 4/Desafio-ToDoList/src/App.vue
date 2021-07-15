eccc3d927a58bbce9296a1b19c6740481e921615
gnome89 <c.olivaresb89@gmail.com>
2021-07-14 20:17:27 -0400

    update

---
+++ Modulo 4/Desafio-ToDoList/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<div id="app">
+<button @click="mostrarComponente">Mostrar o Ocultar</button>
+    <div v-if="mostrar">
+{{ message }}
+<img :src="rutaImagen" />
+<input v-model="message" />
+<helloVue v-if="checked"/>
+  <input type="checkbox" id="checkbox" v-model="checked" />
+    {{ checked }}
+<ul>
+<li v-for="(fruta, index) in frutas" :key="index">
+<span>{{ fruta.name }}</span>
+</li>
+</ul>
+</div>
+</div>
+</template>
+
+
+<script>
+import HelloVue from "./helloVue.vue";
+
+export default {
+name: "App",
+components: {
+HelloVue    
+},
+data() {
+return {
+    frutas: [{ name: "Frutillas"}, {name: "Manzanas"},  {name:"Kiwis"}],
+    mostrar: true,
+message: (Aqui mensaje),
+rutaImagen: 
+"https://assets.pokemon.com/assets/cms2/img/pokedex/full/131.png"
+};
+},
+  methods: {
+    mostrarComponente() {
+      alert("Soy un metodo");
+      this.mostrar = !this.mostrar;
+    }
+  }
+};
+</script>
---
+++ Modulo 4/vuestart/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+<div id="app">
+    <h2>Cantidad de frutas : {{ cantidadDeFrutas }}</h2>
+<button @click="mostrarComponente">Mostrar o Ocultar</button>
+    <div v-if="mostrar">
+{{ message }}
+<img :src="rutaImagen" />
+<input v-model="message" />
+<helloVue v-if="checked"/>
+  <input type="checkbox" id="checkbox" v-model="checked" />
+    {{ checked }}
+<ul>
+<li v-for="(fruta, index) in frutas" :key="index">
+<span>{{ fruta.name }}</span>
+</li>
+</ul>
+</div>
+</div>
+</template>
+
+<script>
+import HelloVue from "./helloVue.vue";
+
+export default {
+name: "App",
+components: {
+HelloVue    
+},
+data() {
+return {
+    frutas: [{ name: "Frutillas"}, {name: "Manzanas"},  {name:"Kiwis"}],
+    mostrar: true,
+message: 'Hola Desafío Latam!!! 🤟🤟',
+rutaImagen: 
+"https://assets.pokemon.com/assets/cms2/img/pokedex/full/131.png"   
+};
+},
+  methods: {
+    mostrarComponente() {
+      alert("Soy un metodo");
+      this.mostrar = !this.mostrar;
+    }
+  }
+};
+</script>
+
+<style>
+.rayado  {
+  text-decoration: line-through;
+}
+</style>
